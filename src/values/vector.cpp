8f8d798bafa682dad85e0cc2265290fe38214475
Jan Krüger <jan.krueger@gmail.com>
2016-03-10 11:35:51 +0100

    Fix rest/next checks on vector seq

--- src/values/vector.cpp
+++ src/values/vector.cpp
@@ -39,24 +39,28 @@ namespace rev {
 
   value_t::p VectorSeq_Seq_first(value_t::p self) {
     auto seq = as<seq_adapter_t<vector_t>>(self)->seq();
-    if (seq) {
+    if (!imu::is_empty(seq)) {
       return seq->first();
     }
     return nullptr;
   }
 
   value_t::p VectorSeq_Seq_rest(value_t::p self) {
-    auto seq  = as<seq_adapter_t<vector_t>>(self)->seq();
-    if (seq) {
-      return imu::nu<seq_adapter_t<vector_t>>(seq->rest());
+    auto seq = as<seq_adapter_t<vector_t>>(self)->seq();
+    if (!imu::is_empty(seq)) {
+      if (auto rest = seq->rest()) {
+        return imu::nu<seq_adapter_t<vector_t>>(rest);
+      }
     }
     return nullptr;
   }
 
   value_t::p VectorSeq_Next_next(value_t::p self) {
-    auto seq  = as<seq_adapter_t<vector_t>>(self)->seq();
-    if (seq) {
-      return imu::nu<seq_adapter_t<vector_t>>(seq->rest());
+    auto seq = as<seq_adapter_t<vector_t>>(self)->seq();
+    if (!imu::is_empty(seq)) {
+      if (auto rest = seq->rest()) {
+        return imu::nu<seq_adapter_t<vector_t>>(rest);
+      }
     }
     return nullptr;
   }
