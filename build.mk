52823b2ab4c1d9af1d73fb665d091905e2a0e47c
Jan Krüger <jan.krueger@gmail.com>
2016-04-23 11:31:38 +0100

    Fix build.mk yet again

--- build.mk
+++ build.mk
@@ -6,22 +6,22 @@ FFI_LFLAGS := $(shell pkg-config --libs libffi)
 products_$(d) := libvm.so
 
 libvm.so_sources_$(d) += \
-    reader.cpp \
-    core.cpp \	
-    values/list.cpp \
-    values/boxed.cpp \
-    values/symbol.cpp \
-    values/vector.cpp \
-    values/map.cpp \
-    values/set.cpp \
-    values/var.cpp \
-    values/ns.cpp \
-    values/binary.cpp \
-    values/string.cpp \
-    values/fn.cpp \
-    values/type.cpp \
-    values/protocol.cpp \
-    values/array.cpp
+	reader.cpp \
+	core.cpp \
+	values/list.cpp \
+	values/boxed.cpp \
+	values/symbol.cpp \
+	values/vector.cpp \
+	values/map.cpp \
+	values/set.cpp \
+	values/var.cpp \
+	values/ns.cpp \
+	values/binary.cpp \
+	values/string.cpp \
+	values/fn.cpp \
+	values/type.cpp \
+	values/protocol.cpp \
+	values/array.cpp
 
 libvm.so_precompiled_$(d) :=
 libvm.so_target_dir_$(d)  := lib
