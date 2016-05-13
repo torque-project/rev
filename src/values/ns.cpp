7ff693289f7f9e191df2930b8c6680a86602006a
Benjamin Teuber <benjamin.teuber@freiheit.com>
2016-05-13 18:32:30 +0100

    Make namespaces printable

--- src/values/ns.cpp
+++ src/values/ns.cpp
@@ -1,7 +1,20 @@
 #include "../values.hpp"
 
 namespace rev {
+  value_t::p Namespace_Printable_str(value_t::p self) {
+    return imu::nu<string_t>(as<ns_t>(self)->name());
+  }
+
+  struct type_t::impl_t Namespace_printable[] = {
+    {0, (intptr_t) Namespace_Printable_str, 0, 0, 0, 0, 0, 0}
+  };
+
+  struct type_t::ext_t Namespace_methods[] = {
+    {protocol_t::str, Namespace_printable}
+  };
+
+  static const uint64_t size = sizeof(Namespace_methods) / sizeof(Namespace_methods[0]);
 
   template<>
-  type_t value_base_t<ns_t>::prototype("Namespace.0");
+  type_t value_base_t<ns_t>::prototype("Namespace.0", Namespace_methods, size);
 }
